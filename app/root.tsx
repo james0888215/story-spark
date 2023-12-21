6baf79278c40886149179845762d27295d736af6
James Kent <jameskent91@gmail.com>
2023-12-21 20:17:08 +0000

    remove showing ENV in request

--- app/root.tsx
+++ app/root.tsx
@@ -18,14 +18,10 @@ export const links: LinksFunction = () => [
 ];
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
-  return json({ user: await getUser(request),  
-    ENV: {
-    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
-  }, });
+  return json({ user: await getUser(request)});
 };
 
 export default function App() {
-  const data = useLoaderData<typeof loader>();
 
   return (
     <html lang="en" className="h-full">
@@ -38,13 +34,6 @@ export default function App() {
       <body className="h-full">
         <Outlet />
         <ScrollRestoration />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `window.ENV = ${JSON.stringify(
-              data.ENV
-            )}`,
-          }}
-        />
         <Scripts />
         <LiveReload />
       </body>
